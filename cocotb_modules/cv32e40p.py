2a8e0acfd74d077480c35bf6d851d0ffdefa536b
Markus Scheck <scheck@esa.tu-darmstadt.de>
2025-02-05 13:39:56 +0100

    moce cocotb modules for cv32e40p here

---
+++ cocotb_modules/cv32e40p.py
@@ -0,0 +1,170 @@
+import cocotb
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+from cocotb.clock import Clock
+import sys
+
+# preload memory model
+memory = [0]*(0x100000)
+mem_bin = open("/scratch/ms/S4E/piccolo_multi_context_support/test_setup/freertos/build/RTOSDemo32.bin", "rb").read()
+#mem_bin = open("/home/wimi/ms/Downloads/tb.bin", "rb").read()
+no_words = int(len(mem_bin)/4)
+no_bytes = int(len(mem_bin))
+for i in range(no_bytes):
+    memory[i] = int(mem_bin[i])
+
+
+async def reset_dut(reset_n, duration_ns):
+    reset_n.value = 0
+    await Timer(duration_ns, units="ns")
+    reset_n.value = 1
+    reset_n._log.debug("Reset complete")
+
+async def memory_sim(dut, dbg_name, obi_prefix, dbg = False):
+    while True:
+        await FallingEdge(dut.clk_i)
+        # check WE if available
+        we = False if obi_prefix != "data" else (dut.data_we_o.value == 1)
+        # read addr
+        if eval(f"dut.{obi_prefix}_req_o.value") == 1:
+            addr = eval(f"dut.{obi_prefix}_addr_o.value") & 0xfffffffc
+            addr_pr = eval(f"dut.{obi_prefix}_addr_o.value")
+            if dbg:
+                print(f"{dbg_name} {"read" if not we else "write"}: 'h{int(addr_pr):08x} {"" if not we else f": 'h{int(dut.data_wdata_o.value):016x} : 'h{int(dut.data_be_o.value):01x}"}")
+                sys.stdout.flush()
+            next_vld = True
+            if not we:
+                try:
+                    be = 0xf if obi_prefix != "data" else (dut.data_be_o.value)
+                    nbl_0 = memory[addr] if (be & 1) != 0 else 0
+                    nbl_1 = memory[addr+1] if (be & 2) != 0 else 0
+                    nbl_2 = memory[addr+2] if (be & 4) != 0 else 0
+                    nbl_3 = memory[addr+3] if (be & 8) != 0 else 0
+                    next_r = nbl_0 + (nbl_1<<8) + (nbl_2<<16) + (nbl_3<<24)
+                    if not we and dbg:
+                        print(f"{dbg_name} read data: 'h{int(next_r):08x} : 'h{int(be):01x}")
+                    sys.stdout.flush()
+                    vld_addr = True
+                except IndexError:
+                    vld_addr = False
+
+            # write
+            if we:
+                for i in range(4):
+                    be = dut.data_be_o.value[i]
+                    addr_w = addr + i
+                    data_w = int(dut.data_wdata_o.value).to_bytes(4, byteorder = 'little')
+                    if be:
+                        try:
+                            memory[addr_w] = data_w[i]
+                        except:
+                            # end simulation
+                            if addr == 0x11004000:
+                                print("got return")
+                                return
+                            else:
+                                print("UNKNOWN ADDRESS")
+        else:
+            next_vld = False
+        await RisingEdge(dut.clk_i)
+        # write data
+        if next_vld:
+            exec(f"dut.{obi_prefix}_rvalid_i.value = 1")
+            if not we and vld_addr:
+                exec(f"dut.{obi_prefix}_rdata_i.value = next_r")
+                if dbg:
+                    #print(f"{dbg_name} read response: {hex(next_r)}")
+                    sys.stdout.flush()
+        else:
+            exec(f"dut.{obi_prefix}_rvalid_i.value = 0")
+
+
+
+async def clint(dut, dbg_name, dbg = False):
+
+    mtime = 0
+    mtime_h = 0
+    mtimecmp = 0
+    mtimecmp_h = 0
+
+    while True:
+        await FallingEdge(dut.clk_i)
+        # check WE if available
+        we = (dut.data_we_o.value == 1)
+        # read addr
+        next_vld = True
+        next_data = 0
+        if dut.data_req_o.value == 1:
+            addr = dut.data_addr_o.value
+            if dbg:
+                print(f"{dbg_name} {"read" if not we else "write"}: {hex(addr)}")
+            if addr == 0x4000BFF8:
+                next_data = mtime
+            elif addr == 0x4000BFFC:
+                next_data = mtime_h
+            elif addr == 0x40004000:
+                next_data = mtimecmp
+            elif addr == 0x40004004:
+                next_data = mtimecmp_h
+            else:
+                next_vld = False
+
+            if we:
+                if addr == 0x4000BFF8:
+                    mtime = dut.data_wdata_o.value
+                elif addr == 0x4000BFFC:
+                    mtime_h = dut.data_wdata_o.value
+                elif addr == 0x40004000:
+                    mtimecmp = dut.data_wdata_o.value
+                elif addr == 0x40004004:
+                    mtimecmp_h = dut.data_wdata_o.value
+
+            
+        await RisingEdge(dut.clk_i)
+        # write data
+        if next_vld:
+            dut.data_rdata_i.value = next_data
+
+        # test condition
+        if ((mtime_h << 32) + mtime) >= ((mtimecmp_h << 32) + mtimecmp):
+            dut.irq_i.value = (1 << 7)
+            if dbg:
+                print("Trigger irq")
+        else:
+            dut.irq_i.value = 0
+
+        # advance counter
+        new_mtime = ((mtime_h << 32) + mtime) + 1
+        mtime = new_mtime & 0xffffffff
+        mtime_h = (new_mtime >> 32) & 0xffffffff
+
+@cocotb.test()
+async def run_program(dut):
+    """Run binary."""
+
+    # tie off unused signals
+    dut.pulp_clock_en_i.value = 1
+    dut.scan_cg_en_i.value = 1
+    dut.boot_addr_i.value = 0
+    dut.mtvec_addr_i.value = 0x100
+    dut.dm_halt_addr_i.value = 0
+    dut.hart_id_i.value = 0
+    dut.dm_exception_addr_i.value = 0
+    dut.debug_req_i.value = 0
+    dut.fetch_enable_i.value = 1
+    dut.irq_i.value = 0
+
+    # memory iface
+    dut.instr_gnt_i.value = 1
+    dut.data_gnt_i.value = 1
+
+    cocotb.start_soon(Clock(dut.clk_i, 1, units="ns").start())
+    cocotb.start_soon(memory_sim(dut, "I", "instr"))
+    cocotb.start_soon(clint(dut, "CLINT", False))
+    mem = cocotb.start_soon(memory_sim(dut, "D", "data"))
+
+    # reset core
+    await reset_dut(dut.rst_ni, 500)
+    dut._log.debug("After reset")
+
+    # wait for finished
+    await mem
---
+++ simulation_wrappers/cv32e40p_tb_top.sv
@@ -0,0 +1,249 @@
+module tb_top (
+    // Clock and Reset
+    input logic clk_i,
+    input logic rst_ni,
+
+    input logic pulp_clock_en_i,  // PULP clock enable (only used if COREV_CLUSTER = 1)
+    input logic scan_cg_en_i,  // Enable all clock gates for testing
+
+    // Core ID, Cluster ID, debug mode halt address and boot address are considered more or less static
+    input logic [31:0] boot_addr_i,
+    input logic [31:0] mtvec_addr_i,
+    input logic [31:0] dm_halt_addr_i,
+    input logic [31:0] hart_id_i,
+    input logic [31:0] dm_exception_addr_i,
+
+    // Instruction memory interface
+    output logic        instr_req_o,
+    input  logic        instr_gnt_i,
+    input  logic        instr_rvalid_i,
+    output logic [31:0] instr_addr_o,
+    input  logic [31:0] instr_rdata_i,
+
+    // Data memory interface
+    output logic        data_req_o,
+    input  logic        data_gnt_i,
+    input  logic        data_rvalid_i,
+    output logic        data_we_o,
+    output logic [ 3:0] data_be_o,
+    output logic [31:0] data_addr_o,
+    output logic [31:0] data_wdata_o,
+    input  logic [31:0] data_rdata_i,
+
+    // Interrupt inputs
+    input  logic [31:0] irq_i,  // CLINT interrupts + CLINT extension interrupts
+    output logic        irq_ack_o,
+    output logic [ 4:0] irq_id_o,
+
+    // Debug Interface
+    input  logic debug_req_i,
+    output logic debug_havereset_o,
+    output logic debug_running_o,
+    output logic debug_halted_o,
+
+    // CPU Control Signals
+    input  logic fetch_enable_i,
+    output logic core_sleep_o
+);
+
+
+// Register Read Logic
+logic [4:0] reg_read_addr_cold_c_to_u;          // reg_read_addr_cold
+logic [31:0] reg_read_data_cold_data_c_to_u;    // reg_read_data_cold_data
+logic EN_reg_read_data_cold_c_to_u;             // EN_reg_read_data_cold
+
+// Register Write Logic
+logic EN_reg_write_cold_c_to_u;                 // EN_reg_write_cold
+logic [36:0] reg_write_cold_u_to_c;             // reg_write_cold
+logic RDY_reg_write_cold_u_to_c;                // RDY_reg_write_cold
+
+// Cold Registers Logic
+logic [927:0] cold_regs_in_c_to_u;              // cold_regs_in
+logic [927:0] cold_regs_w_u_to_c;               // cold_regs_w
+logic RDY_cold_regs_w_u_to_c;                   // RDY_cold_regs_w
+
+// Hot Register Write Logic
+logic [4:0] reg_hot_write_trace_addr_c_to_u;    // reg_hot_write_trace_addr
+logic EN_reg_hot_write_trace_c_to_u;            // EN_reg_hot_write_trace
+
+// Memory Write Logic
+logic EN_mem_wr_c_to_u;                         // EN_mem_wr
+logic [63:0] mem_wr_u_to_c;                     // mem_wr
+logic RDY_mem_wr_u_to_c;                        // RDY_mem_wr
+
+// Memory Read Address Logic
+logic EN_mem_rd_addr_c_to_u;                    // EN_mem_rd_addr
+logic [31:0] mem_rd_addr_u_to_c;                // mem_rd_addr
+logic RDY_mem_rd_addr_u_to_c;                   // RDY_mem_rd_addr
+
+// Memory Read Data Logic
+logic EN_mem_rd_data_c_to_u;                    // EN_mem_rd_data
+logic RDY_mem_rd_data_u_to_c;                   // RDY_mem_rd_data
+logic [31:0] mem_rd_data_d_c_to_u;              // mem_rd_data_d
+
+// Mstatus and Mepc Logic
+logic [31:0] mstatus_out_u_to_c;                // mstatus_out
+logic [31:0] mepc_out_u_to_c;                   // mepc_out
+
+// Bank Switching Logic
+logic switch_bank_o_u_to_c;                     // switch_bank_o
+
+// Mret Logic
+logic EN_mret_c_to_u;                           // EN_mret
+logic mret_u_to_c;                              // mret
+logic RDY_mret_u_to_c;                          // RDY_mret
+
+// CSR Write Logic
+logic write_csrs_u_to_c;                        // write_csrs
+
+// Trap Logic
+logic [31:0] trap_mstatus_c_to_u;               // trap_mstatus
+logic [31:0] trap_mepc_c_to_u;                  // trap_mepc
+logic [31:0] trap_mcause_c_to_u;                // trap_mcause
+logic EN_trap_c_to_u;                           // EN_trap
+logic RDY_trap_u_to_c;                          // RDY_trap
+
+// Custom Instruction Logic
+logic [2:0] custom_inst_funct3_c_to_u;          // custom_inst_funct3
+logic [31:0] custom_inst_id_c_to_u;             // custom_inst_id
+logic [31:0] custom_inst_prio_c_to_u;           // custom_inst_prio
+logic EN_custom_inst_c_to_u;                    // EN_custom_inst
+logic [32:0] custom_inst_u_to_c;                // custom_inst
+logic RDY_custom_inst_u_to_c;                   // RDY_custom_inst
+
+
+cv32e40p_top #(
+    .FPU                      ( 0 ),
+    .FPU_ADDMUL_LAT           ( 0 ),
+    .FPU_OTHERS_LAT           ( 0 ),
+    .ZFINX                    ( 0 ),
+    .COREV_PULP               ( 0 ),
+    .COREV_CLUSTER            ( 0 ),
+    .NUM_MHPMCOUNTERS         ( 1 )
+) u_core (
+    // Clock and reset
+    .rst_ni                   (rst_ni),
+    .clk_i                    (clk_i),
+    .scan_cg_en_i             (scan_cg_en_i),
+
+    // Special control signals
+    .fetch_enable_i           (fetch_enable_i),
+    .pulp_clock_en_i          (pulp_clock_en_i),
+    .core_sleep_o             (core_sleep_o),
+
+    // Configuration
+    .boot_addr_i              (boot_addr_i),
+    .mtvec_addr_i             (mtvec_addr_i),
+    .dm_halt_addr_i           (dm_halt_addr_i),
+    .dm_exception_addr_i      (dm_exception_addr_i),
+    .hart_id_i                (hart_id_i),
+
+    // Instruction memory interface
+    .instr_addr_o             (instr_addr_o),
+    .instr_req_o              (instr_req_o),
+    .instr_gnt_i              (instr_gnt_i),
+    .instr_rvalid_i           (instr_rvalid_i),
+    .instr_rdata_i            (instr_rdata_i),
+
+    // Data memory interface
+    .data_addr_o              (data_addr_o),
+    .data_req_o               (data_req_o),
+    .data_gnt_i               (data_gnt_i),
+    .data_we_o                (data_we_o),
+    .data_be_o                (data_be_o),
+    .data_wdata_o             (data_wdata_o),
+    .data_rvalid_i            (data_rvalid_i),
+    .data_rdata_i             (data_rdata_i),
+
+     // Interrupt interface
+    .irq_i                    (irq_i),
+    .irq_ack_o                (irq_ack_o),
+    .irq_id_o                 (irq_id_o),
+
+    // Debug interface
+    .debug_req_i              (debug_req_i),
+    .debug_havereset_o        (debug_havereset_o),
+    .debug_running_o          (debug_running_o),
+    .debug_halted_o           (debug_halted_o),
+
+    // ctx operations
+    .ctx_en_op_o(EN_custom_inst_c_to_u),
+    .ctx_op_funct3_o(custom_inst_funct3_c_to_u),
+    .ctx_rs1_id_o(custom_inst_id_c_to_u),
+    .ctx_rs2_prio_o(custom_inst_prio_c_to_u),
+    .ctx_rd_write_data_i(custom_inst_u_to_c)
+);
+
+// Instantiate mkRTOSUnitSynth
+mkRTOSUnitSynth u_mkRTOSUnitSynth (
+    .CLK                        (clk_i),                      // Clock input
+    .RST_N                      (rst_ni),                    // Reset input
+
+    // Register Read Logic
+    .reg_read_addr_cold         (reg_read_addr_cold_c_to_u), // reg_read_addr_cold
+    .reg_read_data_cold_data    (reg_read_data_cold_data_c_to_u), // reg_read_data_cold_data
+    .EN_reg_read_data_cold      (EN_reg_read_data_cold_c_to_u), // EN_reg_read_data_cold
+
+    // Register Write Logic
+    .EN_reg_write_cold          (EN_reg_write_cold_c_to_u), // EN_reg_write_cold
+    .reg_write_cold             (reg_write_cold_u_to_c),    // reg_write_cold
+    .RDY_reg_write_cold         (RDY_reg_write_cold_u_to_c), // RDY_reg_write_cold
+
+    // Cold Registers Logic
+    .cold_regs_in               (cold_regs_in_c_to_u),      // cold_regs_in
+    .cold_regs_w                (cold_regs_w_u_to_c),       // cold_regs_w
+    .RDY_cold_regs_w            (RDY_cold_regs_w_u_to_c),   // RDY_cold_regs_w
+
+    // Hot Register Write Logic
+    .reg_hot_write_trace_addr   (reg_hot_write_trace_addr_c_to_u), // reg_hot_write_trace_addr
+    .EN_reg_hot_write_trace     (EN_reg_hot_write_trace_c_to_u), // EN_reg_hot_write_trace
+
+    // Memory Write Logic
+    .EN_mem_wr                  (EN_mem_wr_c_to_u),         // EN_mem_wr
+    .mem_wr                     (mem_wr_u_to_c),            // mem_wr
+    .RDY_mem_wr                 (RDY_mem_wr_u_to_c),        // RDY_mem_wr
+
+    // Memory Read Address Logic
+    .EN_mem_rd_addr             (EN_mem_rd_addr_c_to_u),    // EN_mem_rd_addr
+    .mem_rd_addr                (mem_rd_addr_u_to_c),       // mem_rd_addr
+    .RDY_mem_rd_addr            (RDY_mem_rd_addr_u_to_c),   // RDY_mem_rd_addr
+
+    // Memory Read Data Logic
+    .mem_rd_data_d              (mem_rd_data_d_c_to_u),     // mem_rd_data_d
+    .EN_mem_rd_data             (EN_mem_rd_data_c_to_u),    // EN_mem_rd_data
+    .RDY_mem_rd_data            (RDY_mem_rd_data_u_to_c),   // RDY_mem_rd_data
+
+    // Mstatus and Mepc Logic
+    .mstatus_out                (mstatus_out_u_to_c),       // mstatus_out
+    .mepc_out                   (mepc_out_u_to_c),          // mepc_out
+
+    // Bank Switching Logic
+    .switch_bank_o              (switch_bank_o_u_to_c),     // switch_bank_o
+
+    // Mret Logic
+    .EN_mret                    (EN_mret_c_to_u),           // EN_mret
+    .mret                       (mret_u_to_c),              // mret
+    .RDY_mret                   (RDY_mret_u_to_c),          // RDY_mret
+
+    // CSR Write Logic
+    .write_csrs                 (write_csrs_u_to_c),        // write_csrs
+
+    // Trap Logic
+    .trap_mstatus               (trap_mstatus_c_to_u),      // trap_mstatus
+    .trap_mepc                  (trap_mepc_c_to_u),         // trap_mepc
+    .trap_mcause                (trap_mcause_c_to_u),       // trap_mcause
+    .EN_trap                    (EN_trap_c_to_u),           // EN_trap
+    .RDY_trap                   (RDY_trap_u_to_c),          // RDY_trap
+
+    // Custom Instruction Logic
+    .custom_inst_funct3         (custom_inst_funct3_c_to_u),// custom_inst_funct3
+    .custom_inst_id             (custom_inst_id_c_to_u),    // custom_inst_id
+    .custom_inst_prio           (custom_inst_prio_c_to_u),  // custom_inst_prio
+    .EN_custom_inst             (EN_custom_inst_c_to_u),    // EN_custom_inst
+    .custom_inst                (custom_inst_u_to_c),       // custom_inst
+    .RDY_custom_inst            (RDY_custom_inst_u_to_c)    // RDY_custom_inst
+);
+
+
+
+endmodule
